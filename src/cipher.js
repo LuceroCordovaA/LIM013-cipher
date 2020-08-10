f5ac4eed87f7e19ba32519a1eeb88198fe018a82
LuceroCordovaA <lu.cordovaa@gmail.com>
2020-08-10 11:32:07 -0500

    agregando proyecto

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,41 @@
 const cipher = {
-  // ...
-};
 
+   encode: (desplazamientoParaFrase, frase) => {
+
+      let resultado = ""
+
+      for (let posicion = 0; posicion < frase.length; posicion++) {//.length es para contabilizar los caracteres de esa variable
+         let letra = frase[posicion]; // Devuelve posición a cada letra de la frase
+         let codigoAsciiLetra = letra.charCodeAt(0); // para hallar el código ASCII de cada letra
+
+         if (codigoAsciiLetra >= 65 && codigoAsciiLetra <= 90) {
+            var nuevaPosicion = (codigoAsciiLetra - 65 + desplazamientoParaFrase) % 26 + 65
+         }
+
+         let nuevaLetra = String.fromCharCode(nuevaPosicion)
+
+         resultado = resultado + nuevaLetra;
+      }
+      return resultado;
+   },
+
+   decode: (desplazamientoParaFrase, frase) => {
+   
+      let resultado = ""
+
+      for (let posicion = 0; posicion < frase.length; posicion++) {//.length es para contabilizar los caracteres de esa variable
+         let letra = frase[posicion]; // Devuelve posición a cada letra de la frase
+         let codigoAsciiLetra = letra.charCodeAt(0); // para hallar el código ASCII de cada letra
+
+         if (codigoAsciiLetra >= 65 && codigoAsciiLetra <= 90) {
+            var nuevaPosicion = (codigoAsciiLetra - 90 - desplazamientoParaFrase) % 26 + 90
+         }
+
+         let nuevaLetra = String.fromCharCode(nuevaPosicion)
+
+         resultado = resultado + nuevaLetra;
+      }
+      return resultado;
+   }
+}
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+alert('Colocar una clave. Recuerda compartir tu clave para poder Codificar/Descodificar tu mensaje.')
+
+/*const convertir = document.getElementById('mensajeUsuario');
+convertir.toUpperCase(); Modificar para convertir en Mayúsculas*/
+
+const btnCodificar = document.getElementById('codificar');
+const btnDescodificar = document.getElementById('descodificar');
+const desplazamientoParaFrase = document.getElementById('clave');
+const frase = document.getElementById('mensajeUsuario');
+
+
+function onClickBtnCodificar() {
+    const valueDesplazamientoParaFrase = parseInt(desplazamientoParaFrase.value); //.value para capturar el valor de cada input
+    //parseInt: convierte el valor en número
+    const valueFrase = frase.value.toUpperCase();
+    frase.value = cipher.encode(valueDesplazamientoParaFrase, valueFrase);
+}
+btnCodificar.addEventListener('click', onClickBtnCodificar)
+
+function onClickBtnDescodificar() {
+    const valueDesplazamientoParaFrase = parseInt(desplazamientoParaFrase.value);
+    const valueFrase = frase.value.toUpperCase();
+    frase.value = cipher.decode(valueDesplazamientoParaFrase, valueFrase);
+}
+btnDescodificar.addEventListener('click', onClickBtnDescodificar)
+
+cipher.encode(desplazamientoParaFrase, frase);
+
+cipher.decode(desplazamientoParaFrase, frase);
+
+
+
+
+
